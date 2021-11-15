7f1d42756e778b616765e5fa448f365cf524ebd5
Dmitriy Labud <labud@mail.ru>
2021-11-16 00:11:13 +0300

    Задание 4.3 выполнено

---
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -0,0 +1,49 @@
+package ru.skillbox;
+
+public class Elevator {
+
+    private int maxFloor;
+    private int minFloor;
+    private int curentFloor = 1;
+
+    public Elevator(int minFloor, int maxFloor){
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public int getCurentFloor(){
+        return curentFloor;
+    }
+
+    public void moveDown(){
+        curentFloor -= 1;
+        System.out.println(getCurentFloor());
+    }
+
+    public void moveUp(){
+        curentFloor += 1;
+        System.out.println(getCurentFloor());
+    }
+
+    public void move(int floor){
+        System.out.println("Текущий этаж: " + getCurentFloor());
+        if (floor >= minFloor && floor <= maxFloor){
+            if (curentFloor < floor){
+                for (int i = curentFloor; i < floor; i++){
+                    moveUp();
+                }
+            }
+            else if (curentFloor > floor){
+                for (int i = curentFloor; i > floor; i--){
+                    moveDown();
+                }
+            }
+            else {
+                System.out.println("Вы находитесь на этом этаже!");
+            }
+        }
+        else{
+            System.out.println("Ошибка! Введен неверный этаж!");
+        }
+    }
+}
