321c90d20374a3e80beaeeda45884c09fdeb7855
Dmitriy Labud <labud@mail.ru>
2021-11-20 12:56:05 +0300

    Задание 4.4 Добавил переменную dimentions класса Dimentions, убрал переменную resault.

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Cargo.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Cargo.java
@@ -2,15 +2,15 @@ package ru.skillbox;
 
 public class Cargo {
 
-    private final int resault;
+    Dimensions dimensions;
     private final int weight;
     private final String deliveryAddress;
     private final boolean turnOver;
     private final String registrationNumber;
     private final boolean fragile;
 
-    public Cargo(int resault, int weight, String deliveryAddress, boolean turnOver, String registrationNumber, boolean fragile) {
-        this.resault = resault;
+    public Cargo(Dimensions dimensions, int weight, String deliveryAddress, boolean turnOver, String registrationNumber, boolean fragile) {
+        this.dimensions = dimensions;
         this.weight = weight;
         this.deliveryAddress = deliveryAddress;
         this.turnOver = turnOver;
@@ -19,15 +19,17 @@ public class Cargo {
     }
 
     public Cargo setWeight(int weight) {
-        return new Cargo(resault, weight, deliveryAddress, turnOver, registrationNumber, fragile);
+        return new Cargo(dimensions, weight, deliveryAddress, turnOver, registrationNumber, fragile);
     }
 
     public Cargo setDeliveryAddress(String deliveryAddress) {
-        return new Cargo(resault, weight, deliveryAddress, turnOver, registrationNumber, fragile);
+        return new Cargo(dimensions, weight, deliveryAddress, turnOver, registrationNumber, fragile);
     }
 
-    public Cargo setDimensions(int resault){
-        return new Cargo(resault, weight, deliveryAddress, turnOver, registrationNumber, fragile);
+    public Cargo setDimensions(Dimensions dimensions){
+        return new Cargo(dimensions.setDimensions(dimensions.getWidth(), dimensions.getHeight(),dimensions.getLengh()),
+                weight, deliveryAddress, turnOver, registrationNumber, fragile
+        );
     }
 
     public int getWeight() {
@@ -51,7 +53,7 @@ public class Cargo {
     }
 
     public void print() {
-        System.out.println(resault + " " + getWeight() + " " + getDeliveryAddress() + " " +
+        System.out.println(dimensions.getDimensions() + " " + getWeight() + " " + getDeliveryAddress() + " " +
                             isTurnOver() + " " + getRegistrationNumber() + " " + isFragile());
     }
 }
--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -6,20 +6,26 @@ public class Main {
 
         Dimensions dimensions = new Dimensions(2, 2, 2);
         Dimensions dimensionsCopy = dimensions.setDimensions(3, 3, 3);
-        int resault = dimensionsCopy.getDimensions();
-        Cargo cargo = new Cargo(dimensions.getDimensions(), 20, "Kolasa_street", true,
+
+        Cargo cargo = new Cargo(dimensions, 20, "Kolasa_street", true,
                 "25458AFD25", true);
 
 
         Cargo cargoCopy = cargo.setWeight(25);
         Cargo cargoCopy1 = cargo.setDeliveryAddress("Pobedy street");
-        Cargo cargoCopy2 = cargo.setDimensions(resault);
+        Cargo cargoCopy2 = cargo.setDimensions(dimensionsCopy);
+        System.out.print("CargoCopy: ");
         cargoCopy.print();
+        System.out.print("CargoCopy1: ");
         cargoCopy1.print();
+        System.out.print("CargoCopy2: ");
         cargoCopy2.print();
+        System.out.print("Cargo: ");
         cargo.print();
 
+        System.out.print("DimensionsCopy: ");
         dimensionsCopy.print();
+        System.out.print("Dimensions: ");
         dimensions.print();
     }
 }
