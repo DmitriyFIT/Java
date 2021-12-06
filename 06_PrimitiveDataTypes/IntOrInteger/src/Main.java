a9e6cea9adf8e3dd524c290c333f455824e7aa85
Dmitriy Labud <labud@mail.ru>
2021-12-06 18:30:16 +0300

    Задание выполнено.

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -8,5 +8,17 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        System.out.println("Преобразоване чисел в символы");
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            if ((char) i >= 'А' && (char) i <= 'Я') {
+                System.out.println(i + " - " + (char) i);
+            }
+            if ((char) i >= 'а' && (char) i <= 'я') {
+                System.out.println(i + " - " + (char) i);
+            }
+            if ((char) i == 'Ё' || (char) i == 'ё') {
+                System.out.println(i + " - " + (char) i);
+            }
+        }
     }
 }
