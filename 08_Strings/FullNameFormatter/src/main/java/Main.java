ceb8cbe16b2f21fab13b3f869559b117cda34020
Dmitriy Labud <labud@mail.ru>
2021-12-26 17:01:47 +0300

    9.9 Задание 2

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -12,7 +12,37 @@ public class Main {
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      int countSpace = 0;
+      boolean rightString = true;
+      for (int i = 0; i < input.length(); i++) {
+        if (Character.isSpaceChar(input.charAt(i))) {
+          countSpace++;
+        }
+        if (Character.UnicodeBlock.
+                of(input.charAt(i)).
+                equals(Character.UnicodeBlock.CYRILLIC) ||
+                input.charAt(i) == ' ' ||
+                input.charAt(i) == '-' ||
+                !Character.isDigit(input.charAt(i))) {
+          continue;
+        }
+        else {
+          rightString =false;
+          break;
+        }
+      }
+
+      if (countSpace != 2 || !rightString) {
+        System.out.println("Введенная строка не является ФИО");
+      }
+      else {
+        String surname = input.substring(0, input.indexOf(' '));
+        String name = input.substring(input.indexOf(' '), input.lastIndexOf(' '));
+        String secondName = input.substring(input.lastIndexOf(' '));
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Имя:" + name);
+        System.out.println("Отчество:" + secondName);
+      }
     }
   }
-
 }
