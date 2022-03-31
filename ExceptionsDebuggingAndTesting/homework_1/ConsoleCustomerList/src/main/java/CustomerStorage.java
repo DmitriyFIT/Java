b4f840e20addb28391f61deb8724fde359924caa
Dmitriy Labud <labud@mail.ru>
2022-03-31 22:09:49 +0300

    Homework 14.4 Done

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
@@ -15,6 +17,19 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+
+        String regexPhoneNumber = "\\+[7][0-9]{10}";
+        String regexMail = "^[a-z]*@[a-z]*\\.[a-z]{2,3}$";
+
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong format for ADD");
+        }
+        else if (!components[INDEX_EMAIL].matches(regexMail)) {
+            throw new IllegalArgumentException("Wrong format e-mail");
+        } else if (!components[INDEX_PHONE].matches(regexPhoneNumber)) {
+            throw new IllegalArgumentException("Wrong format phone number");
+        }
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
