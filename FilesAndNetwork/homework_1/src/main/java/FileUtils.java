74900bbfad0eb664aa8fb8aea7161cd7615ec19e
Dmitriy Labud <labud@mail.ru>
2022-07-06 21:18:02 +0300

    Задание 15.2

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
 public class FileUtils {
 
-    public static long calculateFolderSize(String path) {
-        return 0;
+    public static long calculateFolderSize(String path) throws IOException {
+        long size = 0;
+        File folder = new File(path);
+        for (File file : folder.listFiles()) {
+            if (file.isFile()) {
+                size += file.length();
+            }
+            else {
+                size += calculateFolderSize(file.getPath());
+            }
+        }
+        return size;
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
-    }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String path;
+        double size;
+        FileUtils fu = new FileUtils();
+
+        Scanner in = new Scanner(System.in);
+        for (;;) {
+            System.out.println("Введите путь до папки:");
+            path = in.next();
+            size = (double) fu.calculateFolderSize(path);
+
+            int count = 0;
+            while (size > 1024) {
+                 size /= 1024;
+                count++;
+            }
+            String formattedSize = new DecimalFormat("0.00").format(size);
+            String outputConsole = "Размер папки " + path + " составляет: " + formattedSize;
+            switch (count) {
+                case 0:
+                    System.out.println(outputConsole + " байт");
+                    break;
+                case 1:
+                    System.out.println(outputConsole + " Кб");
+                    break;
+                case 2:
+                    System.out.println(outputConsole + " Мб");
+                    break;
+                case 3:
+                    System.out.println(outputConsole + " Гб");
+                    break;
+                case 4 :
+                    System.out.println(outputConsole + " Тб");
+                    break;
+            }
+        }
+     }
 }
