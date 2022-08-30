c70a3d9774f5a73486c3df4df7032b295e36002c
Dmitriy Labud <labud@mail.ru>
2022-08-30 12:15:18 +0300

    Copy folders

--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        String sourcePath;
+        String destinationPath;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter source path folder:");
+        sourcePath = in.next();
+        System.out.println("Enter destination Path folder");
+        destinationPath = in.next();
+        FileUtils.copyFolder(sourcePath, destinationPath);
     }
 }
